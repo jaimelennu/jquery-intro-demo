61ea7ccc93a6dac02ff0ba9f1abbf7b76018cbd3
Jaimelenn <jaimeu@uw.edu>
2014-10-28 09:19:58 -0700

    Change

--- js/app.js
+++ js/app.js
@@ -4,3 +4,39 @@
 
 "use strict";
 
+//select all nav links
+var allNavlinks = $('nav a');
+
+//select all images
+var allImgs = $('img');
+
+function onReady(){
+	//add attribute of target with value bank to each nav link
+	//allNavlinks.attr('target', '_blank');
+	//allNavlinks.addClass('awesomesauce');
+}
+
+allImgs.dblclick(function() {
+	console.log($(this));
+	$(this).addClass('awesomesauce');
+});
+
+allImgs.mouseout(function() {
+	$(this).removeClass('awesomesauce');
+});
+
+allNavlinks.click(function() {
+	//the cooler jquery this - its totally money
+	console.log($(this));
+
+	//the plain version of this 
+	console.log(this);
+
+	//acts o the jQuery object containing this element
+	$(this).addClass('awesomesauce');
+
+
+});
+
+$(onReady);
+
