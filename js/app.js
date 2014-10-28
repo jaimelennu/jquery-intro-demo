fe7d54121d18cb039e074733e574ecc344483e6b
Jaimelenn <jaimeu@uw.edu>
2014-10-28 09:57:35 -0700

    more changes

--- js/app.js
+++ js/app.js
@@ -10,11 +10,23 @@ var allNavlinks = $('nav a');
 //select all images
 var allImgs = $('img');
 
+//select all sections
+var allSections = $('section');
+
 function onReady(){
 	//add attribute of target with value bank to each nav link
 	//allNavlinks.attr('target', '_blank');
 	//allNavlinks.addClass('awesomesauce');
+
+	$('nav').slideUp(3000);
+	$('nav').slideDown();
+	allSections.hide();
+	
 }
+
+$('#exit-button').dblclick(function() {
+	$('#confirm-exit-modal').modal();
+});
 
 allImgs.dblclick(function() {
 	console.log($(this));
@@ -26,15 +38,18 @@ allImgs.mouseout(function() {
 });
 
 allNavlinks.click(function() {
+
 	//the cooler jquery this - its totally money
-	console.log($(this));
+	console.log($(this).attr('href'));
 
 	//the plain version of this 
 	console.log(this);
 
 	//acts o the jQuery object containing this element
-	$(this).addClass('awesomesauce');
-
+	//$(this).addClass('awesomesauce');
+	allSections.hide();
+	$($(this).attr('href')).fadeToggle(2000).addClass('awesomesauce');
+	//method chaining
 
 });
 
